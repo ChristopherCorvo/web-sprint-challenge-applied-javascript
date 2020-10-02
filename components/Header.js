e47153ebb54e53c2cd725075f721ca1efbe68d0c
Christopher Corvo <christophercorvo@gmail.com>
2020-10-02 11:30:35 -0400

    Completed Task 1 Header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+//instantiate variables
+const headerDiv = document.createElement('div');
+const dateSpan =  document.createElement('span');
+const titleH1 = document.createElement('h1');
+const tempSpan = document.createElement('span');
+
+// add class's
+headerDiv.classList.add('header');
+dateSpan.classList.add('date');
+tempSpan.classList.add('temp');
+
+// add content
+dateSpan.textContent = 'MARCH 28, 2020';
+titleH1.textContent = 'Lambda Times';
+tempSpan.textContent = '98°';
+
+//create hiearchy
+headerDiv.append(dateSpan, titleH1, tempSpan)
+
+return headerDiv
+
+}
+
+const mainHeader = document.querySelector('.header-container').append(Header())
+
+
